509d84b5df17b6d80158628b7027c6b51d003eac
asolpshinning <57355937+asolpshinning@users.noreply.github.com>
2022-09-10 21:38:48 +0000

    weird javascript

--- index.js
+++ index.js
@@ -22,20 +22,44 @@ function log(x){
 //5 = 11111 or 14
 
 //S0lution 1
-function isThereSubset(sum, arr){
-  let i = 0
-  while(i < sum){
+/* function isThereSubset(sum, arr = []){
+  let i = 1
+  while(i <= sum){
+    let newArr = arr; 
+    newArr.splice(i,1);
     if(arr.includes(sum)) {log(`array contains ${sum}`); i++; return true}
     else if(sum === 1) {log(`array contains 1 in loop ${i}`); i++; return arr.includes(1)}
     else if(sum === 0) {log(`array contains 0 in loop ${i}`); i++; return true}
-    else if(isThereSubset(i, arr) && isThereSubset(sum - i, arr)){log(`line 30: `, true); i++; return true}
+    else if(arr.includes(i) && isThereSubset(sum - i, newArr)){log(`Running loop ${i} at line 30: ${true}`); i++; return true}
     else {log(`running else`); i++;}; 
   }
+  return 
+} */
 
+function isThereSubset(sum, arr = []){
+ 
+    
+    if(arr.includes(sum)) {log(`array contains ${sum}`);  return true}
+    if(sum === 1) {log(`array contains 1`);  return arr.includes(1)}
+    if(sum === 0) {log(`array contains 0`);  return true}
+
+    let ret = false;
+    let i = 1;
+    let initArr= [0];
+    let newArr;
+    while(i<=sum){
+      
+      newArr = [...arr,1000, 2000]
+      newArr.splice(i,1); log(`At loop ${i}, newArr is ${newArr} and old arr is ${arr}`)
+      if(arr.includes(i) && isThereSubset(sum - i, newArr)){log(`Running loop ${i} at line 52: ${true}`); ret = true; break;}
+      else {log(`running else`); i++ };
+    }
+
+  return ret;
 }
 
-console.log(isThereSubset(10, [1,2,3,4,5]))
-//console.log(isThereSubset(10, [6, 1, 7, 3, 5]));
+//console.log(isThereSubset(10, [1,2,3,4,5]))
+console.log(isThereSubset(19, [6, 1, 3, 6, 5]));
 
 
 
