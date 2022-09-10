ec20ef9911c92e49fc2625907609a7a304893586
asolpshinning <57355937+asolpshinning@users.noreply.github.com>
2022-09-10 14:41:02 +0000

    sum of subset not working yet

--- index.js
+++ index.js
@@ -13,36 +13,46 @@ Input: {1, 3, 4, 8}, S=6
 Output: False
 The given set does not have any subset whose sum is equal to '6'. */
 
+function log(x){
+  console.log(x)
+}
+
 function findSum(x, arr) {
   let itExists = false;
-  let sum;
-  let arrTruths = new Array(x).fill(false);
-  for (let i = 0; i < x; i++) {
-    for (let j = 0; j < arr.length; j++) {
+  let arrTruths = new Array(x).fill(false); //for numbers from 1 to x
+  let i = 0;
+  while ( i < x) {
+    let sum = 0;
+    //start with each element in the array and then add the next number to it until you hit the expected sum, i
+    let j = 0;
+    while(j < arr.length) {
       if (arr[j] === i + 1) {
         arrTruths[i] = true;
+        log(`for sum ${i+1}, ${arr[j]} exists in the array. So there is a subset `)
+        j = arr.length + 1
+        
       } else if (arr[j] < i + 1) {
         sum += arr[j];
         if (sum === i + 1) {
           arrTruths[i] = true;
+          log(`for sum ${i+1}, we have summed some array elements to equal sum. So there is a subset `)
+          j = arr.length + 1
+        } else if(sum > i+1){
+          sum -= arr[j];
         }
+        else {
+          log(`for sum ${i+1}, sum is currently ${sum}... still looking for a complete subset `);
+          j++
       }
+      } else {j = j + 1; console.log(`here right now`)}
     }
+    i = i + 1;
+    console.log(`now going to run i = ${i}`)
   }
   return arrTruths;
 }
+console.log(findSum(4, [5, 2, 3, 7]));
+//console.log(findSum(10, [6, 1, 7, 3, 5]));
 
-const answerArr = (sum, arrQ) => {
-  let i = 0;
-  let arr;
-  while (i < sum) {
-    arr[i] = findSum(sum, arrQ);
-    i++;
-  }
-  return arr;
-};
 
-function getAnswer(sum, arr) {}
 
-console.log(findSum(6, [1, 2, 3, 7]));
-console.log(findSum(10, [1, 6, 7, 1, 5]));
