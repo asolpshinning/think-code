203f1796c7237105a6c5d61f238b44a4f1d0c6ce
Sunday Akins <57355937+asolpshinning@users.noreply.github.com>
2022-09-10 10:39:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const findAns = (arr, expectedAns) => {
+  const findMax = (anArr) => {
+    let max;
+    for (let i=1; i < anArr.length; i++){
+      if(anAnArr[i] > anArr[0])
+    }
+  }
+};
+const expectedAns = 10;
+const inputArr = [6, 3, 4];
+console.log(findAns(inputArr, expectedAns));
