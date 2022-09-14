76f401b698a2ce382cf577d9382904f0a77368ec
asolpshinning <57355937+asolpshinning@users.noreply.github.com>
2022-09-14 02:36:41 +0000

    trying to solve canConcat

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-/* non adjacent sum
-Write a function, nonAdjacentSum, that takes in an array of numbers as an argument. The function should return the maximum sum of non-adjacent elements in the array. There is no limit on how many elements can be taken into the sum as long as they are not adjacent.
+/* can concat
+Write a function, canConcat, that takes in a string and an array of words as arguments. The function should return boolean indicating whether or not it is possible to concatenate words of the array together to form the string.
 
-For example, given:
+You may reuse words of the array as many times as needed.
 
-[2, 4, 5, 12, 7]
-
-The maximum non-adjacent sum is 16, because 4 + 12. 
-4 and 12 are not adjacent in the array.
 test_00:
-const nums = [2, 4, 5, 12, 7];
-nonAdjacentSum(nums); // -> 16
+canConcat("oneisnone", ["one", "none", "is"]); // -> true
 test_01:
-const nums = [7, 5, 5, 12];
-nonAdjacentSum(nums); // -> 19
+canConcat("oneisnone", ["on", "e", "is"]); // -> false
 test_02:
-const nums = [7, 5, 5, 12, 17, 29];
-nonAdjacentSum(nums); // -> 48
+canConcat("oneisnone", ["on", "e", "is", "n"]); // -> true
 test_03:
-const nums = [
-  72, 62, 10,  6, 20, 19, 42,
-  46, 24, 78, 30, 41, 75, 38,
-  23, 28, 66, 55, 12, 17, 9,
-  12, 3, 1, 19, 30, 50, 20
-];
-nonAdjacentSum(nums); // -> 488
+canConcat("foodisgood", ["is", "g", "ood", "f"]); // -> true
 test_04:
-const nums = [
-  72, 62, 10,  6, 20, 19, 42, 46, 24, 78,
-  30, 41, 75, 38, 23, 28, 66, 55, 12, 17,
-  83, 80, 56, 68,  6, 22, 56, 96, 77, 98,
-  61, 20,  0, 76, 53, 74,  8, 22, 92, 37,
-  30, 41, 75, 38, 23, 28, 66, 55, 12, 17,
-  72, 62, 10,  6, 20, 19, 42, 46, 24, 78,
-  42
-];
-nonAdjacentSum(nums); // -> 1465 */
+canConcat("santahat", ["santah", "hat"]); // -> false
+test_05:
+canConcat("santahat", ["santah", "san", "hat", "tahat"]); // -> true
+test_06:
+canConcat("rrrrrrrrrrrrrrrrrrrrrrrrrrx", ["r", "rr", "rrr", "rrrr", "rrrrr", "rrrrrr"]); // -> false
+test_07:
+canConcat("fooisgood", ["foo", "is", "g", "ood", "f"]); // -> true */
 
 
-const nonAdjacentSum = (nums, index = 0, memo = {}) => {
-  // todo
-  if(index in memo) return memo[index];
-  if(nums.length-1 === index) return nums[nums.length-1];
-  if(index >= nums.length) return 0
-  let first = nums[index] + nonAdjacentSum(nums, index + 2, memo);
-  let second = nonAdjacentSum(nums, index + 1, memo);
-  memo[index] =  Math.max(first, second);
-  return memo[index]
+const canConcat = (str, words, i = 0) => {
+  
+  function find(s){
+    let check = '';
+    while(i<s.length){
+      check += s[i]
+      if(words.includes(check)) check = ''
+      i++
+    }
+
+    if(check.length === 0) return true
+    else return false
+  }
+  let ans;
+  let j = 1
+  while(j <= str.length){
+    if(j === str.length) ans = words.includes(str) 
+    else ans = words.includes(str.substring(0,j)) && find(str.substring(j+1, str.length))
+    console.log(str.substring(0,j) + ' ' + str.substring(j+1,str.length) + ' ' + words.includes(str.substring(0,j)) + ' and ' + find(str.substring(j+1, str.length)))
+    //console.log(str.substring(2,5))
+    if (ans === true) return ans;
+    j++
+  }
+  
+  return ans
+  
+  
 };
-console.log(nonAdjacentSum([2, 4, 5, 12, 7]))
+console.log(`Answer is: `, canConcat('heythere', ['h', 'hey', 'the']))
